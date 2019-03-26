54c9616cc1f44a54abff012989befa2a351dd6e9
karla fragoso cerecedo <1717110248@utectulancingo.edu.mx>
2019-03-25 23:46:23 -0600

    first commit

---
+++ app/src/main/java/com/example/karla/emergencycase/Bienvenido.java
@@ -0,0 +1,22 @@
+package com.example.karla.emergencycase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Bienvenido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+        final TextView mensaje = findViewById(R.id.mensaje);
+        Intent i = this.getIntent();
+        String nombre = i.getStringExtra("nombre");
+        int edad = i.getIntExtra("edad",-1);
+        mensaje.setText(mensaje.getText()+" "+nombre+" Su edad:"+edad+"");
+    }
+}
+
+
